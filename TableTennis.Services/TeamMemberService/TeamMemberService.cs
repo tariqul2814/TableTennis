dcdfe727e4e6c81ca321cc1b5f8e7583f1699a1c
tariqul2814 <tariqul2814@gmail.com>
2020-12-16 19:19:53 +0600

    TeamMember

--- TableTennis.Services/TeamMemberService/TeamMemberService.cs
+++ TableTennis.Services/TeamMemberService/TeamMemberService.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace TableTennis.Services.TeamMemberService
 {
-    public class TeamMemberService
+    public class TeamMemberService : ITeamMemberService
     {
         private readonly IUoW _uow;
         public TeamMemberService(IUoW uoW)
@@ -39,62 +39,79 @@ namespace TableTennis.Services.TeamMemberService
 
         public bool CreateOrUpdateTeamMember(TeamMemberDTO teamMemberInsert, string UserId)
         {
-            if(teamMemberInsert.Id>0)
+            try
             {
-                var TeamMember = new TeamMember
+                if (teamMemberInsert.Id > 0)
                 {
-                    Id = teamMemberInsert.Id,
-                    IsRemove = false,
-                    Team = _uow.TeamRepository.Get(x=> x.Id == teamMemberInsert.TeamId && !x.IsRemove).FirstOrDefault(),
-                    TeamId = teamMemberInsert.TeamId,
-                    TeamMemberName = teamMemberInsert.TeamMemberName,
-                    ModifiedBy = UserId,
-                    ModifiedDate = DateTime.Now
-                };
-            }
-            else
-            {
-                var checkTotalMemberInTeam = _uow.TeamMemberRepository.Get(x => x.TeamId == teamMemberInsert.TeamId).ToList();
+                    var checkAvailability = _uow.TeamMemberRepository.Get(x => x.Id == teamMemberInsert.Id && !x.IsRemove).FirstOrDefault();
 
-                if(checkTotalMemberInTeam.Count<3)
+                    if(checkAvailability==null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        var TeamMember = new TeamMember
+                        {
+                            IsRemove = false,
+                            CreateDate = checkAvailability.CreateDate,
+                            CreatedBy = checkAvailability.CreatedBy,
+                            ModifiedBy = UserId,
+                            ModifiedDate = DateTime.Now,
+                            TeamMemberName = teamMemberInsert.TeamMemberName
+                        };
+
+                        _uow.TeamMemberRepository.Update(TeamMember);
+                        _uow.Commit();
+                        return true;
+                    }
+                }
+                else
                 {
                     var TeamMember = new TeamMember
                     {
                         IsRemove = false,
-                        Team = _uow.TeamRepository.Get(x => x.Id == teamMemberInsert.TeamId && !x.IsRemove).FirstOrDefault(),
-                        TeamId = teamMemberInsert.TeamId,
-                        TeamMemberName = teamMemberInsert.TeamMemberName,
+                        CreateDate = DateTime.Now,
                         CreatedBy = UserId,
-                        CreateDate = DateTime.Now
+                        TeamMemberName = teamMemberInsert.TeamMemberName
                     };
-
                     _uow.TeamMemberRepository.Insert(TeamMember);
-                    _uow.TeamMemberRepository.Save();
-
+                    _uow.Commit();
                     return true;
                 }
-
-                else
-                {
-                    return false;
-                }
+            }
+            catch(Exception er)
+            {
+                return false;
             }
         }
 
-        public bool DeleteTeamMember(int TeamMemberId)
+        public bool DeleteTeamMember(int TeamMemberId, string UserId)
         {
-            var checkAvailability = _uow.TeamMemberRepository.Get(x=> x.Id == )
+            var checkAvailability = _uow.TeamMemberRepository.Get(x => x.Id == TeamMemberId && !x.IsRemove).FirstOrDefault();
+            if(checkAvailability!=null)
+            {
+                checkAvailability.IsRemove = false;
+                checkAvailability.ModifiedBy = UserId;
+                _uow.TeamMemberRepository.Update();
+                _uow.Commit();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
     public interface ITeamMemberService
     {
-        List<TeamMemberDTO> GetAllTeamMember();
+        List<TeamMember> GetAllTeamMember();
 
-        TeamMemberDTO GetByTeamMemberId(int TeamMemberId);
+        TeamMember GetByTeamMemberId(int TeamMemberId);
 
-        bool CreateOrUpdateTeamMember(TeamMemberDTO team);
+        bool CreateOrUpdateTeamMember(TeamMemberDTO team, string UserId);
 
-        bool DeleteTeamMember(int TeamMemberId);
+        bool DeleteTeamMember(int TeamMemberId, string UserId);
 
     }
 }
